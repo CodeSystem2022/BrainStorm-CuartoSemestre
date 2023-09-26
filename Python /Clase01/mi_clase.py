d7a6ccba62b536e719fd483d336857c689df979f
GiuntaP <113065894+GiuntaP@users.noreply.github.com>
2023-09-26 16:11:09 -0300

    Create mi_clase.py

---
+++ Python /Clase01/mi_clase.py
@@ -0,0 +1,22 @@
+
+class MiClase:
+    '''
+    Este es un ejemplo de la documentación
+    de esta clase.
+    Es solo para saber que acá esta MiClase.
+    '''
+    def __init__(self):
+        '''
+        Aqui esta el método dunder init
+        Este método es el de inicio
+        para esta clase
+        '''
+
+    def mi_metodo (self, param1, param2):
+        '''
+        Este es el método creado por mi que recibe parámetros
+        Detecto los párametros automaticamente
+        :param param1: Este es el parámetro número 1.
+        :param param2: Este es el parámetro número 2.
+        :return: Este es el valor de retorno.
+        '''
