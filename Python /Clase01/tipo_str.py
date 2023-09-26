a4ba2ab5a262394e8fb63f94ba6fbef150bd13c5
GiuntaP <113065894+GiuntaP@users.noreply.github.com>
2023-09-26 16:07:54 -0300

    Create tipo_str.py

---
+++ Python /Clase01/tipo_str.py
@@ -0,0 +1,13 @@
+import math
+
+# Profundizamos en el tipo String
+# Concatenacion automatica en Python
+
+variable = ' Adios'
+mensaje = 'Hola'' Alumnos '+variable
+mensaje += ', Terminamos '
+print(mensaje)
+
+# Usamos la clase help para ayudar o documentación (bulit-it)
+
+help(math.isnan)
