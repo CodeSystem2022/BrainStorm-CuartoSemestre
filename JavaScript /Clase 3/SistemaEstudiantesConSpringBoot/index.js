86257efd913fc11fe611664006be4655d6aec334
Franco Leone <112661380+franco8787@users.noreply.github.com>
2023-10-01 13:12:47 -0300

    Create index.js

---
+++ JavaScript /Clase 3/SistemaEstudiantesConSpringBoot/index.js
@@ -0,0 +1,39 @@
+const shopContent = document.getElementById("shopContent");
+const cart = [];
+
+productos.forEach((product) =>{
+        const content = document.createElement("div");
+        content.className = "card";
+        content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.productName}</h3>
+        <p class="price">${product.price} $</p>
+        `;
+        shopContent.append(content);
+
+        const buyButton = document.createElement("button");
+        buyButton.innerText = "Buy";
+
+        content.append(buyButton);
+
+        buyButton.addEventListener("click", () => {
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+         
+        if (repeat) {
+         cart.map((prod.id === product.id) {
+            if (prod.id === product.id) {
+               prod.quanty++;
+            }
+         })
+        }else {
+         cart.push({
+           id: product.id,
+           productName: product.productName,
+           price: product.price,
+           quanty: product.quanty,
+           img: product.img,
+        });
+      }
+     });
+  });
+  
